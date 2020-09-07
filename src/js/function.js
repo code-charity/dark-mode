ae9328255800091bf86d71846d33a496f0a49714
Victor Savinov <katashiroku@gmail.com>
2020-09-07 22:39:15 +0300

    Updated "Title" element

--- src/js/function.js
+++ src/js/function.js
@@ -6,6 +6,8 @@ function update(container) {
     if (!Satus.isset(container)) {
         container = document.querySelector('.satus-main__container');
     }
+    
+    document.querySelector('.satus-text--title').innerText = Satus.locale.getMessage(this.history[this.history.length - 1].label) || 'Dark Mode';
 
     document.body.dataset.appearance = id;
     container.dataset.appearance = id;
